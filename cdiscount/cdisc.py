21e6f80ca30636b94ef0d690b52adc086dde4df2
pydevdse <pydev.dse@gmail.com>
2021-12-29 21:47:24 +0200

    add proxy to splash

--- cdiscount/cdisc.py
+++ cdiscount/cdisc.py
@@ -9,41 +9,50 @@ from flask import jsonify
 logging.basicConfig(level=logging.INFO)
 
 
-class Cdiscount_requests():
-    
+class Cdiscount_requests:
     def get_session(self, url, proxy=None):
-        #self.sess = requests.Session()
+        # self.sess = requests.Session()
         session = requests.session()
         session.verify = False
         session.headers = {
-            'Accept': '*/*',
-            'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language': 'en-US;q=0.8,en;q=0.5',
-            'Connection': 'keep-alive',
-            'DNT': '1',
-            'Host': url.split('//')[-1].split('/')[0],
-            'User-Agent' : USER_AGENT[randint(0, len(USER_AGENT)-1)]
+            "Accept": "*/*",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "en-US;q=0.8,en;q=0.5",
+            "Connection": "keep-alive",
+            "DNT": "1",
+            "Host": url.split("//")[-1].split("/")[0],
+            "User-Agent": USER_AGENT[randint(0, len(USER_AGENT) - 1)],
         }
         if not proxy:
             return session
 
-        proxies = {'http':proxy,'https':proxy}
-        session.proxies=proxies
+        proxies = {"http": proxy, "https": proxy}
+        session.proxies = proxies
         return session
-        
+
     def parse(self, url, proxy):
         session = self.get_session(url)
-        splash_url = 'http://splash:8050/render.html'
-        response = session.get(splash_url, params={'url': url, 'response_body': 1, 'cookies': True,
-                                           'timeout': 10, 'wait': 5,  'js_enabled': True,
-                                           'redirect': True, 'User-Agent' : USER_AGENT[randint(0, len(USER_AGENT)-1)]})
+        splash_url = "http://splash:8050/render.html"
+        response = session.get(
+            splash_url,
+            params={
+                "url": url,
+                "response_body": 1,
+                "cookies": True,
+                "proxy": proxy,
+                "timeout": 10,
+                "wait": 5,
+                "js_enabled": True,
+                "redirect": True,
+                "User-Agent": USER_AGENT[randint(0, len(USER_AGENT) - 1)],
+            },
+        )
         try:
-           tree = html.fromstring(response.content)
+            tree = html.fromstring(response.content)
         except Exception as e:
             logging.info(f"Error tree {e}")
-            return {'error':e}
+            return {"error": e}
         jsons = []
         for j in tree.xpath('//script[@type="application/ld+json"]/text()'):
             jsons.append(json.loads(j.strip()))
-        return {"jsons": jsons, 'html': response.text}
-
+        return {"jsons": jsons, "html": response.text}
--- cdiscount/cdisc_flask.py
+++ cdiscount/cdisc_flask.py
@@ -1,6 +1,6 @@
 import logging
 import json
-from flask import Flask,  request, jsonify
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api, abort
 from flask_caching import Cache
 from cdisc import Cdiscount_requests
@@ -8,9 +8,9 @@ from cdisc import Cdiscount_requests
 logging.basicConfig(level=logging.INFO)
 
 config = {
-    "DEBUG": True,          # some Flask specific configs
+    "DEBUG": True,  # some Flask specific configs
     "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
-    "CACHE_DEFAULT_TIMEOUT": 300
+    "CACHE_DEFAULT_TIMEOUT": 300,
 }
 
 response = Cdiscount_requests()
@@ -25,14 +25,14 @@ class Cdis_parse(Resource):
     def url_proxy(self):
         if request.get_json():
             logging.info(f" JSON {request.json}")
-            url = request.json.get('url')
-            proxy = request.json.get('proxy')
+            url = request.json.get("url")
+            proxy = request.json.get("proxy")
             if not url:
                 abort(400, error="URL not found in JSON")
             return url, proxy
         args = request.args
-        if args.get('url'):
-            url = args.get('url')
+        if args.get("url"):
+            url = args.get("url")
         else:
             abort(400, error="URL not found in args")
         if args.get("proxy"):
@@ -40,7 +40,7 @@ class Cdis_parse(Resource):
         else:
             proxy = None
         return url, proxy
-        
+
     def get(self):
         url, proxy = self.url_proxy()
         logging.info(f"Proxy {proxy} ")
@@ -49,8 +49,8 @@ class Cdis_parse(Resource):
         if isinstance(r, dict):
             if r.get("error"):
                 return jsonify(error=str(r.get("error")))
-        return jsonify(jsons=r['jsons'], html=r['html'])# jsonify(html=str(html))#
-        
+        return jsonify(jsons=r["jsons"], html=r["html"])  # jsonify(html=str(html))#
+
     def post(self):
         url, proxy = self.url_proxy()
         logging.info(f"Proxy {proxy} ")
@@ -59,9 +59,10 @@ class Cdis_parse(Resource):
         if isinstance(r, dict):
             if r.get("error"):
                 return jsonify(error=str(r.get("error")))
-        return jsonify(jsons=r['jsons'], html=r['html'])# jsonify(html=str(html))#
+        return jsonify(jsons=r["jsons"], html=r["html"])  # jsonify(html=str(html))#
 
-api.add_resource(Cdis_parse, '/parser/cdiscount/')
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+api.add_resource(Cdis_parse, "/parser/cdiscount/")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
